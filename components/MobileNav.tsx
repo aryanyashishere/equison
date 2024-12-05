36ab7a811d395ee588ec8e0aebcb12e07d47f98b
Aryan Yash <aryanyashishere@gmail.com>
2024-12-05 08:47:52 +0530

    reponsive connect bank in left sidebar

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -15,6 +15,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import Footer from "./Footer"
+import PlaidLink from "./PlaidLink"
 
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathname = usePathname();
@@ -69,7 +70,9 @@ const MobileNav = ({ user }: MobileNavProps) => {
                 )
               })}
 
-              USER
+              {/* USER */}
+              <PlaidLink user={user} /> 
+
               </nav>
             </SheetClose>
 
